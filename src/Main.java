8a4280b80636d34bd071fe7a58d17d87355397f3
Omar Amin <omar_75@hotmail.dk>
2019-02-19 23:00:10 +0100

    Small adjustments

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,36 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
 
-        CardFlip c = new CardFlip(10);
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("How many cards do you want to play with?");
+        int numCards = 0;
         while (sc.hasNext()){
             try {
-                c.runGame(sc.nextInt());
+                numCards = sc.nextInt();
+                break;
             }catch (InputMismatchException e){
                 System.out.println("WRONG INPUT!!!");
                 sc.next();
             }
         }
+
+        CardFlip c = new CardFlip(numCards);
+
+        System.out.println("Type 'exit' if you want to leave this game");
+        while (sc.hasNext()){
+            try {
+                c.runGame(sc.nextInt());
+            }catch (InputMismatchException e){
+                if(sc.next().equals("exit")){
+                    break;
+                }
+                System.out.println("WRONG INPUT!!!");
+            }
+        }
+
     }
 }
